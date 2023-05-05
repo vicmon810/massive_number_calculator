bf25a161c92bb5abd26bb4ee93abcb8dbe69fc6e
crire90p <reuben.crimp@otago.ac.nz>
2023-05-06 00:09:59 +1200

    added etude files

---
+++ Integer.cpp
@@ -0,0 +1,112 @@
+#include "Integer.h"
+
+namespace cosc326 {
+
+	Integer::Integer() {
+	
+	}
+
+	Integer::Integer(const Integer& i) {
+	
+	}
+
+	Integer::Integer(const std::string& s) {
+	
+	}
+
+
+	Integer::~Integer() {
+	
+	}
+
+	Integer& Integer::operator=(const Integer& i) {
+		return *this;
+	}
+
+	Integer Integer::operator-() const {
+		return Integer(*this);
+	}
+
+	Integer Integer::operator+() const {
+		return Integer(*this);
+	}
+
+	Integer& Integer::operator+=(const Integer& i) {
+		return *this;
+	}
+
+	Integer& Integer::operator-=(const Integer& i) {
+		return *this;
+	}
+
+	Integer& Integer::operator*=(const Integer& i) {
+		return *this;
+	}
+
+	Integer& Integer::operator/=(const Integer& i) {
+		return *this;
+	}
+
+	Integer& Integer::operator%=(const Integer& i) {
+		return *this;
+	}
+
+	Integer operator+(const Integer& lhs, const Integer& rhs) {
+		return lhs;
+	}
+
+	Integer operator-(const Integer& lhs, const Integer& rhs) {
+		return lhs;
+	}
+
+	Integer operator*(const Integer& lhs, const Integer& rhs) {
+		return lhs;
+	}
+
+	Integer operator/(const Integer& lhs, const Integer& rhs) {
+		return lhs;
+	}
+
+	Integer operator%(const Integer& lhs, const Integer& rhs) {
+		return lhs;
+	}
+
+
+	std::ostream& operator<<(std::ostream& os, const Integer& i) {
+		return os;
+	}
+
+	std::istream& operator>>(std::istream& is, Integer& i) {
+		return is;
+	}
+
+	bool operator<(const Integer& lhs, const Integer& rhs) {
+		return true;
+	}
+
+	bool operator> (const Integer& lhs, const Integer& rhs) {
+		return true;
+	}
+
+	bool operator<=(const Integer& lhs, const Integer& rhs) {
+		return true;
+	}
+
+	bool operator>=(const Integer& lhs, const Integer& rhs) {
+		return true;
+	}
+
+	bool operator==(const Integer& lhs, const Integer& rhs) {
+		return true;
+	}
+
+	bool operator!=(const Integer& lhs, const Integer& rhs) {
+		return true;
+	}
+
+
+	Integer gcd(const Integer& a, const Integer& b) {
+		return a;
+	}
+
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+	Test main for Rational and Integer
+*/
+
+#include <iostream>
+using namespace std;
+
+#include "Integer.h"
+#include "Rational.h"
+
+using namespace cosc326;
+
+int main() {
+	std::cout << "Kia ora World" << std::endl;
+
+	return 0;
+}
